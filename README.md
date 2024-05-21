44cb28bc826cd11912912017a7665a7279474670
maksak-ann <anna.breeva@mail.ru>
2024-05-21 20:18:15 +0300

    Ready to test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/maksak-ann/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const template = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const list = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+const createCard = (card, callbackOnDelete) => {
+  const element = template.querySelector(".card").cloneNode(true);
+
+  element.querySelector(".card__image").src = card.link;
+  element.querySelector(".card__image").alt = card.name;
+  element.querySelector(".card__title").textContent = card.name;
+
+  list.append(element);
+
+  const deleteButton = element.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", (event) => {
+    callbackOnDelete(element);
+  });
+
+  return element;
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (card) => {
+  card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  createCard(card, deleteCard);
+});
